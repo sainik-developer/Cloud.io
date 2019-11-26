a484049974568a0f3d96f587cd9e8e7096d7152e
sainik <schattapadhyay@ask-fast.com>
2019-11-26 14:04:51 +0530

    login response dto added

---
+++ src/main/java/com/cloudio/rest/dto/LoginResponseDTO.java
@@ -0,0 +1,18 @@
+package com.cloudio.rest.dto;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class LoginResponseDTO {
+    @JsonProperty(value = "Authorization")
+    private String authorization;
+}
