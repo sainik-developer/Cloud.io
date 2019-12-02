8d0839c576c0d9c629291c90c8f9c12f8dd81230
sainik <schattapadhyay@ask-fast.com>
2019-12-02 18:45:36 +0530

    validation error advice added

--- src/main/java/com/cloudio/rest/advice/GroupErrorAdvice.java
+++ src/main/java/com/cloudio/rest/advice/GroupErrorAdvice.java
@@ -3,9 +3,16 @@ package com.cloudio.rest.advice;
 import com.cloudio.rest.dto.ResponseDTO;
 import com.cloudio.rest.exception.*;
 import org.springframework.http.HttpStatus;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @RestControllerAdvice
 public class GroupErrorAdvice {
@@ -56,5 +63,18 @@ public class GroupErrorAdvice {
     @ResponseStatus(HttpStatus.CONFLICT)
     public ResponseDTO companyNameNotUniqueException(final Exception e) {
         return new ResponseDTO(null, "Company name already in use", null);
+    }
+
+    @ExceptionHandler({MethodArgumentNotValidException.class})
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public Map<String, String> yourExceptionHandler(MethodArgumentNotValidException e) {
+        Map<String, String> errors = new HashMap<>();
+
+        BindingResult bindingResult = e.getBindingResult();
+        List<FieldError> fieldErrors = bindingResult.getFieldErrors();
+        for (FieldError fieldError : fieldErrors) {
+            errors.put(fieldError.getField(), fieldError.getDefaultMessage());
+        }
+        return errors;
     }
 }
