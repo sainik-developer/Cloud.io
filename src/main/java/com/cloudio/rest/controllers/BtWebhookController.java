371d6c836e3f290328a0cac5bded658d3dbb4cfa
sainik <schattapadhyay@ask-fast.com>
2019-12-13 13:52:35 +0530

    changed webhook end point

--- src/main/java/com/cloudio/rest/controllers/BtWebhookController.java
+++ src/main/java/com/cloudio/rest/controllers/BtWebhookController.java
@@ -2,9 +2,11 @@ package com.cloudio.rest.controllers;
 
 import com.braintreegateway.BraintreeGateway;
 import com.braintreegateway.WebhookNotification;
+import com.cloudio.rest.dto.BrainTreeWebHookRequestDTO;
 import com.cloudio.rest.service.PaymentService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -19,15 +21,15 @@ public class BtWebhookController {
     private final BraintreeGateway gateway;
     private final PaymentService paymentService;
 
-    @PostMapping("/failed")
-    void someMethod(@RequestParam("bt_signature") final String btSignature, @RequestParam("bt_payload") final String btPayload) {
+    @PostMapping(value = "/failed", consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE)
+    void someMethod(BrainTreeWebHookRequestDTO webHookRequestDTO) {
 //        final WebhookNotification webhookNotification = gateway.webhookNotification().parse(btSignature, btPayload);
 //        log.info("Webhook notification kind is {}", webhookNotification.getKind());
 //        log.info("Webhook notification details are {}", webhookNotification.toString());
 //        log.info("");
 //        webhookNotification.getSubscription().getId();// subscription ID
 
-        log.info("Braintree bt_signature={} and bt_payload={}", btSignature, btPayload);
+        log.info("Braintree bt_signature={} and bt_payload={}", webHookRequestDTO.getBtSignature(), webHookRequestDTO.getBtPayload());
 
     }
 }
