754cf6f6c2b04dd06007b6e78a440eef69ff9c8c
sainik <schattapadhyay@ask-fast.com>
2020-01-13 10:51:57 +0530

    open api doc is fixed

--- src/main/java/com/cloudio/rest/config/OpenApiConfig.java
+++ src/main/java/com/cloudio/rest/config/OpenApiConfig.java
@@ -3,14 +3,13 @@ package com.cloudio.rest.config;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Contact;
-import io.swagger.v3.oas.annotations.info.Info;
 import io.swagger.v3.oas.annotations.servers.Server;
 
 @OpenAPIDefinition(
-        info = @Info(
-                title = "Qring API",
+        info = @io.swagger.v3.oas.annotations.info.Info(
+                title = "Cloud.io API",
                 version = "v1",
-                description = "This app provides REST APIs for qring.eq",
+                description = "This app provides REST APIs for Cloud.io",
                 contact = @Contact(
                         name = "sainik",
                         email = "schattapadhyay@ask-fast.com"
