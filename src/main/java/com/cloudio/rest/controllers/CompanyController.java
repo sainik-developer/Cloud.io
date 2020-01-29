f87882c35a012b414fc3b5186d0283ae6714841b
sainik <schattapadhyay@ask-fast.com>
2020-01-29 17:46:50 +0530

    members issue in company create state is resolved

--- src/main/java/com/cloudio/rest/controllers/CompanyController.java
+++ src/main/java/com/cloudio/rest/controllers/CompanyController.java
@@ -138,16 +138,17 @@ public class CompanyController {
                 .map(CompanyMapper.INSTANCE::fromDTO)
                 .flatMap(companyRepository::save)
                 .map(CompanyMapper.INSTANCE::toDTO)
-                .flatMap(companyDto -> groupService.createDefaultGroup(companyDto.getCompanyId())
-                        .doOnNext(groupDo -> log.info("Default group is created and details are {}", groupDo))
-                        .map(GroupMapper.INSTANCE::toDTO)
-                        .map(groupDto -> {
-                            companyDto.setGroups(Collections.singletonList(groupDto));
-                            return companyDto;
-                        })
-                )
-                .flatMap(companyDto -> accountService.createAccount(companyDto.getCompanyId(), authService.decodeTempAuthToken(authorizationToken).getPhoneNumber(), AccountType.ADMIN, null, null)
-                        .map(accountDto -> companyDto))
+                .flatMap(companyDto -> accountService.createAccount(companyDto.getCompanyId(),
+                        authService.decodeTempAuthToken(authorizationToken).getPhoneNumber(),
+                        AccountType.ADMIN, null, null)
+                        .flatMap(accountDo -> groupService.createDefaultGroup(companyDto.getCompanyId())
+                                .doOnNext(groupDo -> log.info("Default group is created and details are {}", groupDo))
+                                .map(GroupMapper.INSTANCE::toDTO)
+                                .map(groupDto -> {
+                                    groupDto.setMembers(Collections.singletonList(AccountMapper.INSTANCE.toDTO(accountDo)));
+                                    companyDto.setGroups(Collections.singletonList(groupDto));
+                                    return companyDto;
+                                })))
                 .switchIfEmpty(Mono.error(new InvalidTempTokenException("Temp token is invalid")));
     }
 
