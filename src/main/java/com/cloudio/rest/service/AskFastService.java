50b33bdc6151a113cd8bc3a73c839601f2f87c71
akumar@ask-fast.com <akumar@ask-fast.com>
2020-02-17 13:28:11 +0530

    minnor change

--- src/main/java/com/cloudio/rest/service/AskFastService.java
+++ src/main/java/com/cloudio/rest/service/AskFastService.java
@@ -4,6 +4,4 @@ import reactor.core.publisher.Mono;
 
 public interface AskFastService {
     Mono<Boolean> doAuthAndSendSMS(final String phoneNumber, final String smsContent);
-
-    //Mono<Boolean> doAuthAndSendEmail(final String toEmailAddress, final String emailContent);
 }
